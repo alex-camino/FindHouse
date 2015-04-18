4e3951821ec641f9a6149978ac22f3a445571f0a
alex-camino <alex.camino.89@gmail.com>
2015-04-18 13:23:59 +0200

    Prueba

--- src/clases/Conexiones.java
+++ src/clases/Conexiones.java
@@ -2,11 +2,8 @@ package clases;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import com.mysql.jdbc.Statement;
 
 public class Conexiones {
 
--- src/clases/Main.java
+++ src/clases/Main.java
@@ -1,5 +1,9 @@
 package clases;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Main {
 
 	public static long inicioSegundamano, inicioMilanuncios, finalSegundamano, finalMilanuncios;
@@ -7,9 +11,9 @@ public class Main {
 		
 		//Llamamos a la clase Segundamano, para hacerle el Scraping
 		
-		
+		/*
 		Segundamano scrapingSegundamano = new Segundamano();
-		//Milanuncios scrapingMilanuncios = new Milanuncios();
+		Milanuncios scrapingMilanuncios = new Milanuncios();
 		
 		
 		try {
@@ -25,7 +29,25 @@ public class Main {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		*/
+		Connection conexion=Conexiones.realizarConexion();
 		
+		PreparedStatement pstmt;
+		
+		try {
+			pstmt = conexion.prepareStatement("insert into inmuebles(in_titulo) values(?)");
+			pstmt.setString(1, "Esto es una prueba");
+			pstmt.executeUpdate();
+			
+			pstmt.close();
+		
+		}catch(SQLException ex){
+			
+			System.out.println("Error al insertar el inmueble en la Base de Datos");
+			System.out.println(ex.getMessage()+"\n"+ex.getErrorCode());
+		}
+			
+		System.out.println("HOLA MUNDO");
 		
 		//ZilyoApi api = new ZilyoApi();
 		//api.iniciarApi();
